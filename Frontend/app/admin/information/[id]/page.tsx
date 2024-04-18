a82402f519cd8ca8ab7be0c5d831dff1ae620820
Aquaturner <paritleo@gmail.com>
2024-04-19 00:10:53 +0700

    Update page.tsx

--- Frontend/app/admin/information/[id]/page.tsx
+++ Frontend/app/admin/information/[id]/page.tsx
@@ -4,6 +4,7 @@ import { useState,useEffect } from "react";
 import { ProviderData } from "@/types"
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import PictureParser from "@/components/PictureParser";
 
 export default function page({params} : {params: {id: string}}) {
 
@@ -34,32 +35,40 @@ export default function page({params} : {params: {id: string}}) {
     if(!information) return null
 
     return (
-        <div className="w-1/2 mt-24 bg-slate-100 m-5 p-5 rounded-lg flex flex-col justify-center">
-            <div className="text-xl text-center font-bold capitalize m-5">
-                {information?.name}
+        <div className="mt-24 w-full flex flex-col justify-center items-center">
+            <div className="text-xl font-bold uppercase tracking-widest">
+                Information
             </div>
-            <div className="flex flex-col text-lg table-auto border-separate border-spacing-2">
-                
-                <div className="flex justify-center mb-2">
-                    <Image src={information?.picture} alt="provider picture" width={100} height={100} className="rounded-lg"/>
+            <div className="mt-12 flex justify-center">
+                <Image src={PictureParser(information?.picture)} alt="provider picture" width={200} height={200} className="rounded-lg"/>
+            </div>
+            <div className="w-1/3 bg-gray-100 m-5 p-5 rounded-lg flex flex-col justify-center shadow-md font-semibold">
+                <table className="w-full table-auto border-separate border-spacing-2">
+                    <tbody>
+                        <tr>
+                            <td className="text-gray-500 uppercase">Name</td>
+                            <td className="text-right">{information?.name}</td>
+                        </tr>
+                        <tr>
+                            <td className="text-gray-500 uppercase">Address</td>
+                            <td className="text-right">{information?.address}</td>
+                        </tr>
+                        <tr>
+                            <td className="text-gray-500 uppercase">Contact</td>
+                            <td className="text-right">{information?.contact}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <hr className="border-gray-300 m-4" />
+                <div className="mx-2 mb-4 flex flex-col justify-center">
+                    <div className="mb-2">Citizen card</div>
+                    <Image src={PictureParser(information?.citizenCard)} alt="provider picture" width={300} height={300} className="rounded-lg"/>
                 </div>
-                <div className="flex flex-row">
-                    <p>Address : </p>
-                    <p className="text-right">{information?.address}</p>
+                <hr className="border-gray-300 m-4" />
+                <div className="mx-2 mb-4 flex flex-col justify-center">
+                    <div className="mb-2">Citizen certificate</div>
+                    <Image src={PictureParser(information?.citizenCertificate)} alt="provider picture" width={500} height={500} className="rounded-lg"/>
                 </div>
-                <div className="flex flex-row">
-                    <p>Contact : </p>
-                    <p className="text-right">{information?.contact}</p>
-                </div>
-                <div className="flex flex-col justify-center mb-2">
-                    <p>Citizen card : </p>
-                    <Image src={information?.citizenCard} alt="provider picture" width={300} height={300} className="rounded-lg"/>
-                </div>
-                <div className="flex flex-col justify-center mb-2">
-                    <p>Citizen certificate : </p>
-                    <Image src={information?.citizenCertificate} alt="provider picture" width={500} height={500} className="rounded-lg"/>
-                </div>
-               
             </div>
         </div>
     );
