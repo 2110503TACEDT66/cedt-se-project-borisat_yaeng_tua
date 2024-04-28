51fa7309d52471814758dcc7f6360dc890d1433d
Aquaturner <paritleo@gmail.com>
2024-04-28 23:19:06 +0700

    Update page.tsx

--- Frontend/app/payment/page.tsx
+++ Frontend/app/payment/page.tsx
@@ -90,7 +90,7 @@ export default function Payment() {
                                             <div className="w-1/3 flex flex-col bg-white rounded-xl p-8 m-3">
                                                 <div className="flex flex-row justify-between items-center">
                                                     <div className="font-bold text-xl">
-                                                        {paymentHistory.information.name}
+                                                        Payment Details
                                                     </div>
                                                     <button onClick={toggleModal} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
                                                         <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -100,7 +100,7 @@ export default function Payment() {
                                                 </div>
                                                 <div className="mt-7 p-8 flex flex-row justify-between bg-gray-100 rounded-xl">
                                                     <div>
-                                                        TANBOON TIANGTUT
+                                                        {paymentHistory.information.name}
                                                     </div>
                                                     <div>
                                                         7 Jul 24
